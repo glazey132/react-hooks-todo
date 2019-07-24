1aad135ec9581fd6b7364bb584870b1bcbc7a3a6
glazey132 <alglaze132@gmail.com>
2019-07-23 18:14:59 -0700

    import hooks and helpers into loginn form

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,16 +1,44 @@
 import * as React from 'react';
 import { Button, Form, FormGroup, Input } from 'reactstrap';
 
+/** Presentation */
+import ErrorMessage from '../components/ErrorMessage';
+
+/** Custom Hooks */
+import useErrorHandler from '../utils/custom-hooks/ErrorHandler';
+
+/** Utils */
+import { apiRequest, validateLoginForm } from '../utils/Helpers';
+import { Header } from '../components/Styles'; 
+
 function Login(){
     const [userEmail, setUserEmail] = React.useState('');
     const [userPassword, setUserPassword] = React.useState('');
     const [loading, setLoading] = React.useState('');
+    const { error, showError } = useErrorHandler(null);
+
+    const authHandler = async () => {
+        try {
+            setLoading(true);
+            const userData = await apiRequest(
+                "https://jsonplaceholder.typicode.com/users",
+                "post",
+                { email: userEmail, password: userPassword }
+            );
+            const { id, email } = userData;
+        } catch(err) {
+            setLoading(false);
+            showError(err.message);
+        }
+    }
 
     return (
         <Form
             onSubmit={e => {
                 e.preventDefault();
-                // Auth Handler
+                if (validateLoginForm(userEmail, userPassword, showError)) {
+                    authHandler();
+                }
             }}
         >
             <Header>Sign In</Header>
@@ -33,8 +61,10 @@ function Login(){
                 />
             </FormGroup>
             <Button type="submit" disabled={loading} block={true}>
-            {loading ? "Loading..." : "Sign In"}
+                {loading ? "Loading..." : "Sign In"}
             </Button>
+            <br />
+            {error && <ErrorMessage errorMessage={error} />}
         </Form>
     );
 }
